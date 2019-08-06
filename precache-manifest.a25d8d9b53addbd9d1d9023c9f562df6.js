58fbc0eb2ab954d18d44f5bf46c882b1a8a9e562
hsuanya129 <37055177+hsuanya129@users.noreply.github.com>
2019-08-06 17:19:45 +0800

    Updates

---
+++ precache-manifest.a25d8d9b53addbd9d1d9023c9f562df6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a171c1a98420848a57f5e3a6a9d286",
+    "url": "/webcam_manipulation_canvas/index.html"
+  },
+  {
+    "revision": "208e8976791f2bda7243",
+    "url": "/webcam_manipulation_canvas/static/css/main.84cf5dc1.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/webcam_manipulation_canvas/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "208e8976791f2bda7243",
+    "url": "/webcam_manipulation_canvas/static/js/main.49ebe05c.chunk.js"
+  },
+  {
+    "revision": "88338fb3879b6bbd3809",
+    "url": "/webcam_manipulation_canvas/static/js/runtime~main.3f965d41.js"
+  }
+]);
